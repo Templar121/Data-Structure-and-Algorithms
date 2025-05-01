d01b8e4eea2dd7455594a1f26a74e8901d4909a5
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-05-01 13:02:40 +0530

    first commit

---
+++ Leetcode/Leetcode - 2239/Find_Closest_Number_to_Zero.py
@@ -0,0 +1,10 @@
+def closestnum(nums):
+    closest = nums[0]
+    for num in nums:
+        if abs(num) < abs(closest):
+            closest = num
+        
+    if closest < 0 and abs(closest) in nums:
+        return abs(closest)
+    else:
+        return closest
