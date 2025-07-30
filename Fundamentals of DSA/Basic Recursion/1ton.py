7d6949600d2d4297ce6b518a37a66da7b1b07d71
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-30 08:46:48 +0530

    Add alternative recursion function for 1 to n printing
    Introduced recursion2, an alternative implementation using a nested helper function to print numbers from 1 to n. This provides another approach to demonstrate basic recursion concepts.

--- Fundamentals of DSA/Basic Recursion/1ton.py
+++ Fundamentals of DSA/Basic Recursion/1ton.py
@@ -7,3 +7,13 @@ def recursion(i, n):
     recursion(i + 1, n)
     
 recursion(1, n)    
+
+def recursion2(n):
+    def helper(i):
+        if i > n:
+            return
+        print(i)
+        helper(i + 1)
+    helper(1)
+    
+recursion2(n)
