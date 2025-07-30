812d3a60cd05b51ac91740ed034a68fa88c1daa2
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-30 10:57:51 +0530

    Add recursive Fibonacci sequence generator
    Introduces a Python script that calculates the nth Fibonacci number using a basic recursive function. The script prompts the user for input and prints the result.

---
+++ Fundamentals of DSA/Basic Recursion/fibonacci.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number of Fibonacci terms you want to generate: "))
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+print(fibonacci(n))
