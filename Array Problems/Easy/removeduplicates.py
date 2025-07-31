d1aacd7fc8725e1db82c2bbe2ab204255d04f351
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-31 22:26:19 +0530

    Create removeduplicates.py

---
+++ Array Problems/Easy/removeduplicates.py
@@ -0,0 +1,16 @@
+nums = list(map(int, input("Please Enter the elements in the Array Sperated by Space: ").split()))
+i = 0
+j = 0
+n = len(nums)
+
+if not nums:
+    print(0)
+else:
+    i = 0
+    for j in range(1, n):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+            
+print(f"Number of Unique elements in the array {nums} is {i + 1}")
+print(f"Modified Array with uniq elements is {nums[:i + 1]}")
